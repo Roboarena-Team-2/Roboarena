fd34b528b77522e9a32a36021480d0c5d99cb0cb
Sarah <sarah.herty@student.uni-tuebingen.de>
2025-05-08 12:56:56 +0200

    add long test line

--- src/main.py
+++ src/main.py
@@ -1,6 +1,10 @@
 import pygame
 import sys
 
+# longe line to test that flake8 now accepts files that are formatted with black
+result = 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10 + 11 + 12 + 13 + 14 + 15 + 16 + 17 + 18 + 19 + 20 + 21 + 22 + 23 + 24 + 25
+print("Das Ergebnis ist:", result)
+
 # Initialisierung
 pygame.init()
 pygame.mixer.init()
