883eb34ce51b90e0b7026e5529cf277faf6b8ddb
Katja102 <katja.grammel@student.uni-tuebingen.de>
2025-05-08 14:06:06 +0200

    Add movement for player

--- src/main.py
+++ src/main.py
@@ -7,6 +7,7 @@ from robot import Robot
 
 # Initialization
 pygame.init()
+clock = pygame.time.Clock()
 
 # Create window
 screen = pygame.display.set_mode((config.WIDTH, config.HEIGHT))
@@ -21,15 +22,31 @@ robot1 = Robot(screen, 500, 500, 20, 180, (255, 255, 255))
 robot2 = Robot(screen, 800, 300, 30, 0, (0, 100, 190))
 robot3 = Robot(screen, 300, 600, 40, 50, (255, 50, 120))
 
+player = robot1
+
+
+# Movement
+vel = 2
+turn_speed = 5
+
+
 # Game loop
 running = True
 while running:
+    clock.tick(60)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         elif event.type == pygame.KEYDOWN:
+            print(pygame.key.name(event.key))
             if event.key == pygame.K_ESCAPE:
                 running = False
+
+    keys = pygame.key.get_pressed()
+
+    player.x += (keys[pygame.K_RIGHT] - keys[pygame.K_LEFT]) * vel
+    player.y += (keys[pygame.K_DOWN] - keys[pygame.K_UP]) * vel
+    player.alpha += (keys[pygame.K_d] - keys[pygame.K_a]) * turn_speed
 
     screen.fill((220, 220, 220))  # light gray background
     arena.draw_map()
