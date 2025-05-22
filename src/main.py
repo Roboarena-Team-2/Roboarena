8bcda08ddceabfd03f79bc2764cee3f3f07cc149
Katja102 <katja.grammel@student.uni-tuebingen.de>
2025-05-22 07:40:04 +0200

    create movement for other robots

--- src/main.py
+++ src/main.py
@@ -17,16 +17,13 @@ clock = pygame.time.Clock()
 arena = Arena(screen, config.ROWS, config.COLUMNS, map.COLORS)
 arena.create_map(map.get_map1())
 
-robot1 = Robot(screen, 500, 500, 20, 180, (255, 255, 255))
-robot2 = Robot(screen, 800, 300, 30, 0, (0, 100, 190))
-robot3 = Robot(screen, 300, 600, 40, 50, (255, 50, 120))
+player = Robot(screen, 500, 500, 20, 180, (255, 255, 255), 1, 1)
+enemy1 = Robot(screen, 800, 300, 30, 0, (0, 100, 190), 1, 1)
+enemy2 = Robot(screen, 300, 600, 40, 50, (255, 50, 120), 1, 1)
+enemy3 = Robot(screen, 1200, 600, 40, 50, (0, 250, 0), 1, 1)
 
-player = robot1
-
-
-# Movement
-vel = 2
-turn_speed = 5
+circle_tick = 50
+angle = 180
 
 
 # Game loop
@@ -39,18 +36,18 @@ while running:
             if event.key == pygame.K_ESCAPE:
                 running = False
 
-    keys = pygame.key.get_pressed()
-
-    player.x += (keys[pygame.K_RIGHT] - keys[pygame.K_LEFT]) * vel
-    player.y += (keys[pygame.K_DOWN] - keys[pygame.K_UP]) * vel
-    player.alpha += (keys[pygame.K_d] - keys[pygame.K_a]) * turn_speed
-
     screen.fill((220, 220, 220))  # light gray background
     arena.draw_map()
-    robot1.draw_robot()
-    robot2.draw_robot()
-    robot3.draw_robot()
+    ticks = pygame.time.get_ticks()
+    player.update_player()
+    if ticks > circle_tick:
+        circle_tick += 50
+        angle = (angle + 3) % 360
+    enemy1.move_circle([800,300],50,angle)
+    enemy2.update_enemy(player)
+    enemy3.update_enemy(player)
     pygame.display.flip()
+    clock.tick(60)
 
 # Cleanup
 pygame.quit()
--- src/robot.py
+++ src/robot.py
@@ -1,15 +1,28 @@
 import pygame
 import math
+import numpy as np
+import config
 
+BOUNDS_X = [0, config.WIDTH]
+BOUNDS_Y = [0, config.HEIGHT]
+
+a_max = 1
+a_alpha_max = 1
 
 class Robot:
-    def __init__(self, screen, x, y, radius, direction, color):
+    def __init__(self, screen, x, y, radius, direction, color, a, a_alpha):
         self.screen = screen
         self.x = x  # x-coordiante of center
         self.y = y  # y-coordinate of center
         self.r = radius  # radius of circle
         self.alpha = direction  # direction of the robot
         self.color = color  # color of the robot
+        self.a = a  # current acceleration for moving
+        self.a_alpha = a_alpha  # current acceleration for turning
+        self.a_max = a_max  # maximal acceleration for moving    (constant in program)
+        self.a_alpha_max = a_alpha_max  # maximal acceleration for turning     (constant in program)
+        self.v = 1  # speed for moving
+        self.v_alpha = 1  # speed for turning
 
     def draw_robot(self):
         # draw robot (circle)
@@ -37,3 +50,38 @@ class Robot:
                            (left_eye_x, left_eye_y), eye_radius)
         pygame.draw.circle(self.screen, (0, 0, 0),
                            (right_eye_x, right_eye_y), eye_radius)
+        
+
+    def update_player(self):
+        self.draw_robot()
+
+        keys = pygame.key.get_pressed()
+
+        self.x += (keys[pygame.K_RIGHT] - keys[pygame.K_LEFT]) * self.v
+        self.y += (keys[pygame.K_DOWN] - keys[pygame.K_UP]) * self.v
+        self.alpha += (keys[pygame.K_d] - keys[pygame.K_a]) * self.v_alpha
+
+        self.x = max(BOUNDS_X[0], min(self.x, BOUNDS_X[1] - self.r))
+        self.y = max(BOUNDS_Y[0], min(self.y, BOUNDS_Y[1] - self.r))
+
+
+    def update_enemy(self, player):
+        x_to_goal = player.x-self.x
+        y_to_goal = player.y-self.y
+        self.x += math.copysign(self.v, x_to_goal)  / 3
+        self.y += math.copysign(self.v, y_to_goal)  / 3
+        angle_to_player = math.degrees(math.atan2(y_to_goal,x_to_goal))+180%360
+        if(angle_to_player<self.alpha):
+            if(abs(angle_to_player-self.alpha)>180):
+                angle_to_player *= -1
+        else:
+            if(abs(angle_to_player-self.alpha)<180):
+                angle_to_player *= -1
+        self.alpha += math.copysign(self.v_alpha, angle_to_player)
+        self.draw_robot()
+
+    def move_circle(self,point,r,angle):
+        self.x = point[0] + r * math.cos(angle*math.pi/180)
+        self.y = point[1] + r * math.sin(angle*math.pi/180)
+        self.alpha = angle + 90
+        self.draw_robot()
